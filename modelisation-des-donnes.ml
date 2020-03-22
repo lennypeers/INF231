0c8b616da441d745d1ccc3bda3b28ceb5e251873
lennypeers <lenny.peers@gmail.com>
2020-03-22 15:05:21 +0100

    fun en_ordre working

--- modelisation-des-donnes.ml
+++ modelisation-des-donnes.ml
@@ -33,6 +33,42 @@ type main = tuile multiensemble ;;
 type pioche = tuile multiensemble ;;
 
 
+(* Q7 *)
+
+let en_ordre (ens:tuile multiensemble) : tuile multiensemble =
+
+    let comp_ordre (a,occ1: tuile multielement) (b,occ2: tuile multielement) : bool =
+        match (a,b) with
+            | (_,Joker) -> true
+            | (T(n1,Bleu),T(n2,couleur)) -> (couleur <> Bleu) || n1 < n2
+            | (T(n1,Rouge),T(n2,couleur)) -> couleur = Jaune || couleur = Noir || (couleur = Rouge && n1 < n2) 
+            | (T(n1,Jaune),T(n2,couleur)) -> couleur = Noir || (couleur = Jaune && n1 < n2)
+            | (T(n1,Noir),T(n2,couleur)) -> couleur = Noir && n1 < n2
+            | _ -> false in
+
+    let rec ordonne (ens: tuile multiensemble) (temp: tuile multiensemble) : tuile multiensemble =
+        match ens with 
+            | [] -> temp
+            | head1::tail1 -> match temp with
+                                | [] -> ordonne tail1 [head1]
+                                | head2::tail2 -> if (comp_ordre head1 head2)
+                                                  then ordonne tail1 (head1::head2::tail2)
+                                                  else ordonne ([head1]@tail1@[head2]) tail2
+    in ordonne ens [] ;;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
