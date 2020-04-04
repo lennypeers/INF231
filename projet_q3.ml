6f4cd6cb714ddfcc473cff10300887c6af0c2db8
joshnah <57731922+joshnah@users.noreply.github.com>
2020-04-04 21:03:20 +0200

    Update projet_q3.ml

--- projet_q3.ml
+++ projet_q3.ml
@@ -17,8 +17,6 @@ let nbocc(s:'a multiensemble)(e:'a):int=
 let appartient(s:'a multiensemble)(e:'a):bool=
   List.fold_left(fun z (x,y) -> x=e|| z) false s;;
 
-let inclus(s1:'a multiensemble)(s2:'a multiensemble):bool=
-  List.fold_left(fun z (x,y) -> appartient s2 x && z) true s1;;
 
 let ajout(s:'a multiensemble)((a,b):'a multielement):'a multiensemble=
   if appartient s a then List.fold_right (fun (x,y) z -> if x=a then (x,y+b)::z else z) s []
