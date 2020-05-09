1b95abb4c2b2c1d899dced1f69a43d08b8d4a24a
joshnah <57731922+joshnah@users.noreply.github.com>
2020-05-09 20:34:44 +0200

    Update projet_end.ml

--- projet_end.ml
+++ projet_end.ml
@@ -255,6 +255,10 @@ let tableVmens (table: table) : tuile multiensemble =
 
 (* Q13 *)
 
+(* Conditions for the first coup:
+- combinaisons in p are valide
+- difference m0 m1 = tuile ensemble of p
+- point of the pose is >= 30 *)
 let premier_coup_ok (m0: main) (p :pose) (m1: main) : bool =
     (combinaisons_valides p) && (difference m0 m1 = tableVmens p) && (points_pose p >= 30) ;;
 
@@ -262,11 +266,12 @@ let premier_coup_ok (m0: main) (p :pose) (m1: main) : bool =
 
 
 
-(*  Conditions for a coup (not the first one) 
-- t1 is valide
+(* Conditions for a coup (not the first one):
+- combinaisons in t1 are valide
 - m0 <> m1 
 - t0 includes t1
-- difference t1 t0 = difference  m0 m1  *)
+- difference t1 t0 = difference  m0 m1 *)
+
 let coup_ok (t0: table) (m0: main) (t1: table) (m1: main) : bool =
     (combinaisons_valides t1) && 
     let diff_main = difference m0 m1 and t1 = tableVmens t1 and t0 = tableVmens t0 in
